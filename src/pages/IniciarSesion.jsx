1ae787c181f943ceecd9dccab23f6252bc5c1069
danna duque <dannadeilydc@ufps.edu.co>
2022-11-25 20:54:04 -0500

    iniciar sesion

--- src/pages/IniciarSesion.jsx
+++ src/pages/IniciarSesion.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import { Link } from "react-router-dom";
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 
+import React, { useEffect,useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import { Link } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import useAuth from "../hooks/useAuth";
+import conexionAxios from "../config/axios";
+import Alerta from "../components/alerta";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -49,8 +53,36 @@ const useStyles = makeStyles((theme) => ({
 export default function SignInSide() {
   const classes = useStyles();
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const {setAuth, } = useAuth();
+      
+    
+  
+    const handleSubmit = async e => {
+      e.preventDefault();
+  
+      try { 
+          const { data } = await conexionAxios.post('/login', {email, password})
+          // setAlerta({})
+          localStorage.setItem('token', data.token)
+          setAuth(data)
+         
+      } catch (error) {
+          // setAlerta({
+          //     msg: error.response.data.msg,
+          //     error: true
+          // })
+      }
+  
+  }
+  
+    const [alerta, setAlerta] = useState({});
+    const { msg } = alerta;
+
   return (
-    <Grid container component="main" className={classes.root}>
+    <Grid container component="main" className={classes.root} >
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -61,7 +93,8 @@ export default function SignInSide() {
           <Typography component="h1" variant="h5">
             INICIAR SESION
           </Typography>
-          <form className={classes.form} noValidate>
+          {msg && <Alerta alerta={alerta } />}
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
             <TextField
               variant="outlined"
               margin="normal"
@@ -72,6 +105,8 @@ export default function SignInSide() {
               name="email"
               autoComplete="email"
               autoFocus
+              value={email}
+             onChange={ e => setEmail(e.target.value)}
             />
             <TextField
               variant="outlined"
@@ -83,6 +118,8 @@ export default function SignInSide() {
               type="password"
               id="password"
               autoComplete="current-password"
+              value={password}
+              onChange={ e => setPassword(e.target.value)}
             />
            
             <Button
@@ -111,82 +148,8 @@ export default function SignInSide() {
 }
 
 
-// import React, { useEffect,useState } from "react";
-// import Avatar from "@material-ui/core/Avatar";
-// import Button from "@material-ui/core/Button";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import TextField from "@material-ui/core/TextField";
-// import { Link } from "react-router-dom";
-// import Paper from "@material-ui/core/Paper";
-// import Grid from "@material-ui/core/Grid";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-// import Typography from "@material-ui/core/Typography";
-// import { makeStyles } from "@material-ui/core/styles";
-// import FormControl from "@material-ui/core/FormControl";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import MenuItem from "@material-ui/core/MenuItem";
-// import Select from "@material-ui/core/Select";
-// import useAuth from "../hooks/useAuth";
-// import conexionAxios from "../config/axios";
-// import Alerta from "../components/alerta";
 
-
-
-// export default function IniciarSesion() {
-//   const classes = useStyles();
-
-
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-  
-//   const {setAuth, } = useAuth();
-    
-  
-
-//   const handleSubmit = async e => {
-//     e.preventDefault();
-
-//     if([email, password].includes('')) {
-//         setAlerta({
-//             msg: 'Todos los campos son obligatorios',
-//             error: true
-//         });
-//         return
-//     }
-
-
-
-//     try { 
-//         const { data } = await conexionAxios.post('/login', {email, password})
-//         // setAlerta({})
-//         localStorage.setItem('token', data.token)
-//         setAuth(data)
-       
-//     } catch (error) {
-//         // setAlerta({
-//         //     msg: error.response.data.msg,
-//         //     error: true
-//         // })
-//     }
-
-// }
-
-//   const [alerta, setAlerta] = useState({});
-//   const { msg } = alerta;
-
-//   return (
-//     <Grid container component="main" className={classes.root} >
-//       <CssBaseline />
-//       <Grid item xs={false} sm={4} md={7} className={classes.image} />
-//       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-//         <div className={classes.paper}>
-//           <Avatar className={classes.avatar}>
-//             <LockOutlinedIcon />
-//           </Avatar>
-//           <Typography component="h1" variant="h5">
-//             INICIAR SESION
-//           </Typography>
-//           {msg && <Alerta alerta={alerta } />}
+//           
 
 //           <form className={classes.form} noValidate onSubmit={handleSubmit}>
 //             {/* <FormControl variant="outlined" className={classes.formControl}>
@@ -205,55 +168,4 @@ export default function SignInSide() {
 //               </Select>
 //             </FormControl> */}
             
-//             <TextField
-            
-//               variant="outlined"
-//               margin="normal"
-//               required
-//               fullWidth
-//               id="email"
-//               label="Email Address"
-//               name="email"
-//               autoComplete="email"
-//               autoFocus
-              
-//               value={email}
-//               onChange={ e => setEmail(e.target.value)}
-             
-//             />
-//             {msg && <Alerta alerta={alerta.email } />}
-//             <TextField
-//               variant="outlined"
-//               margin="normal"
-//               required
-//               fullWidth
-//               name="password"
-//               label="Password"
-//               type="password"
-//               id="password"
-//               autoComplete="current-password"
-//               value={password}
-//                     onChange={ e => setPassword(e.target.value)}
-              
-//             />
 
-//             <Button
-//               type="submit"
-//               fullWidth
-//               variant="contained"
-//               color="primary"
-//               className={classes.submit}
-//             >
-//               Iniciar sesion
-//             </Button>
-//           </form>
-//           <Grid item>
-//             <Button>
-//               <Link to="/RecuperarPassword">Recuperar contraseña</Link>
-//             </Button>
-//           </Grid>
-//         </div>
-//       </Grid>
-//     </Grid>
-//   );
-// }
