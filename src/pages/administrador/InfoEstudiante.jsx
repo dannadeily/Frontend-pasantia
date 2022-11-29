cf65f355b70ca8f4cb0dd11eddc0481a0a1a6152
danna duque <dannadeilydc@ufps.edu.co>
2022-11-29 10:48:56 -0500

    Actualizacion de info estudiante

--- src/pages/administrador/InfoEstudiante.jsx
+++ src/pages/administrador/InfoEstudiante.jsx
@@ -6,7 +6,13 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-
+import Grid from "@material-ui/core/Grid";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -90,17 +96,252 @@ export default function InfoEstudiante() {
           <LinkTab label="Documentos Finales" href="/spam" {...a11yProps(3)} />
         </Tabs>
       </AppBar>
+      {/* TABLA DE INFORMACION GENERAL  */}
       <TabPanel value={value} index={0}>
-        Page One
+      
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Nombres:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Danna Deily </Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Apellidos:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Duque Conde</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Fecha de nacimiento:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">29/04/2000</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Lugar de nacimiento:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">cúcuta</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Sexo:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Femenino</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Direccion:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">av 4 # 6-47 san luis</Typography>
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Telefono:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">3177141689</Typography>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Cedula:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">1010075720</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Tipo:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Cedula de ciudadania</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Expedida:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Cucuta</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Email:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">dannadeilydc@ufps.edu.co</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Codigo estudiantil:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">1151732</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Semestre:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">10</Typography>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Grid>
+      </Grid>
       </TabPanel>
+
+      {/* DOCUMENTO DE ACEPTACION */}
       <TabPanel value={value} index={1}>
-        Page Two
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Documento de identidad:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Danna Deily </Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Formato solicitud pasantia PPS01:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Duque Conde</Typography>
+                </TableCell>
+              </TableRow>  
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Plan de trabajo pasantia PPS03: </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Danna Deily </Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Convenio "Borrador" PPS02:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Duque Conde</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Acta de Compromiso PPS03-1:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Duque Conde</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Presentacion Plan de trabajo PPS04:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">29/04/2000</Typography>
+                </TableCell>
+              </TableRow>  
+            </TableBody>
+          </Table>
+        </Grid>
+      </Grid>
       </TabPanel>
+
+   {/* AVANCES */}
       <TabPanel value={value} index={2}>
-        Page Three
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Table>
+            <TableBody>
+            <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Informe parcial 1 PPS06: </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Danna Deily </Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Informe parcial 2 PPS06:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">Duque Conde</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Informe final PPS08:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">29/04/2000</Typography>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Grid>
+      </Grid>
       </TabPanel>
+
+    {/* DOCUMENTOS FINALES */}
       <TabPanel value={value} index={3}>
-        Page Three
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Table>
+            <TableBody>
+            <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Solicitud de sustentacion informe final PPS08-1:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">29/04/2000</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">Acta de finalizacion por parte de la empresa PPS08-2:</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="p">29/04/2000</Typography>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Grid>
+      </Grid>
       </TabPanel>
     </div>
   );
