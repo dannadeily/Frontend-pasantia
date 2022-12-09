6cfd23f2c68f446324065deea293d51f82210c0f
adrean130320 <adrean130320@gmail.com>
2022-12-09 14:30:10 -0500

    Se añaden cambiar contraseña, y editar datos

--- src/pages/administrador/CrearDocumento.jsx
+++ src/pages/administrador/CrearDocumento.jsx
@@ -1,29 +1,22 @@
-import React, { useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
+import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import conexionAxios from "../../config/axios";
-
+import InputLabel from "@material-ui/core/InputLabel";
 import Snackbar from "@material-ui/core/Snackbar";
-import MuiAlert from "@material-ui/lab/Alert";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+
 import Alerta from "../../components/alerta";
-import { withStyles } from "@material-ui/core/styles";
-import { green } from "@material-ui/core/colors";
-import FormGroup from "@material-ui/core/FormGroup";
-import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
-import CheckBoxIcon from "@material-ui/icons/CheckBox";
-import Favorite from "@material-ui/icons/Favorite";
-import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -69,6 +62,7 @@ export default function RegistrarJurado() {
   const [documento, setData] = useState({
     nombreDocumento: "",
     aprobacion: "",
+    formato:""
   });
   const [archivo, guardarArchivo] = useState("");
 
@@ -92,6 +86,7 @@ export default function RegistrarJurado() {
     const formData = new FormData();
     formData.append("plantilla", archivo);
     formData.append("nombre", documento.nombreDocumento);
+    formData.append("formato", documento.formato);
     formData.append("aprobacion", state.aprobacion);
     await conexionAxios.post("/documento", formData, {
       headers: {
@@ -108,6 +103,18 @@ export default function RegistrarJurado() {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
+  const [tiposFormatos, listaTiposFormatos] = useState([]);
+
+  const consultarTiposFormatos = async () => {
+    const tiposDocumentosConsulta = await conexionAxios.get("/tipoFormato");
+    //volovar en el state
+    listaTiposFormatos(tiposDocumentosConsulta.data.tiposFormatos);
+  };
+
+  useEffect(() => {
+    consultarTiposFormatos();
+  }, []);
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -118,7 +125,7 @@ export default function RegistrarJurado() {
         <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
           <div>{msg && <Alerta alerta={alerta} />}</div>
         </Snackbar>
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+        <form className={classes.form} onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
@@ -146,7 +153,25 @@ export default function RegistrarJurado() {
                 onChange={leerArchivo}
               />
             </Grid>
-
+            <Grid item xs={12}>
+              <FormControl fullWidth required className={classes.formControl}>
+                <InputLabel id="demo-simple-select-required-label">
+                  Age
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-required-label"
+                  onChange={actualizarState}
+                  className={classes.selectEmpty}
+                  fullWidth
+                  name="formato"
+                >
+                  {tiposFormatos.map((formatos) => {
+                    return (<MenuItem value={formatos.idformato}>{formatos.formato}</MenuItem>);
+                  })}
+                </Select>
+                <FormHelperText>Required</FormHelperText>
+              </FormControl>
+            </Grid>
             <Grid item xs={12}>
               <FormControlLabel
                 control={
